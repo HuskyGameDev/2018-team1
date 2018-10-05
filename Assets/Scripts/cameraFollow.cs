6b504bf50fa9ab1118647e4ee193bb054f9e1c63
Crystal Fletcher <sporeluc@gmail.com>
2018-10-05 16:22:02 -0400

    Implementation of smoothedCamera
    Tied camera to player. Added smoothness factor so that camera has slight "lag" or follow time. 1 is default decent follow time. 0 disables the follow.

---
+++ Assets/Scripts/cameraFollow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class cameraFollow : MonoBehaviour {
+
+    public Transform target;
+
+    public float smoothSpeed;
+    public Vector3 offset;
+
+    private void LateUpdate()
+    {
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothedPosition = desiredPosition;
+        if (smoothSpeed != 0f)
+        {
+            // Times 8 so that 1 is the default decent smoothing
+            smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, ((Time.deltaTime/smoothSpeed) * 8));
+        }
+        
+        transform.position = smoothedPosition;
+    }
+}
