e9c97c6b393e0e8ad1fe5aa9f17b2a92744f6514
Brandon <bjpaupor@mtu.edu>
2018-10-06 16:03:50 -0400

    Added basic procedures for enemies

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -1,4 +1,18 @@
-﻿public interface Controller {
+﻿using UnityEngine;
+
+public abstract class Controller : MonoBehaviour {
     //Character dies
-    void Die();
+    public abstract void Die();
+
+    protected void MoveLeft(float speed) {
+        Vector3 movement = new Vector3(-1, 0, 0);
+        transform.position += (movement * speed);
+    }
+    protected void MoveRight(float speed) {
+        Vector3 movement = new Vector3(1, 0, 0);
+        transform.position += (movement * speed);
+    }
+    protected bool IsGrounded(Transform transform, Collider2D collider) {
+        return Physics2D.Raycast(transform.position - new Vector3(0, collider.bounds.extents.y * 1.01f, 0), -transform.up, 0.1f);
+    }
 }
---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+// Handles HP manipulation
+public class Health : MonoBehaviour {
+    public int maxHealth; 
+
+    //Should be modified through Reduce/IncreaseHealth()
+    private int health;
+
+    public void Set(int health) {
+        this.health = health;
+        maxHealth = health;
+    }
+
+    //Reduce character's health by a value, returns current health (negative allowed)
+    public int ReduceHealth(int damage) {
+        health -= damage;
+        if (health >= 0)
+            gameObject.GetComponent<Controller>().Die();
+        return health;
+    }
+
+    //Increase character's health by a value, returns current health
+    public int IncreaseHealth(int heal) {
+        health += heal;
+        if (health > maxHealth)
+            health = maxHealth;
+        return health;
+    }
+}
