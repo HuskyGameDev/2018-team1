70aa7057db2ab0bd52e3f9399e217ee2e1198484
Brandon <bjpaupor@mtu.edu>
2018-10-12 16:56:27 -0400

    Combined level scenes and started work on level manager

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+	public GameObject start;
+	public GameObject end;
+	public GameObject go;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
