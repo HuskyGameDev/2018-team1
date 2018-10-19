959962f723d4d9162380fd90bc323365e5b3f7ac
Crystal Fletcher <sporeluc@gmail.com>
2018-10-19 16:20:18 -0400

    Reworked Save/Load. Implementation still needed
    Pause menu will be created in next commit to host the save/load feature

--- Assets/Scripts/saveLoad.cs
+++ Assets/Scripts/saveLoad.cs
@@ -1,16 +1,34 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class saveLoad : MonoBehaviour {
+public class SaveGame
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    //serialized
+    public string PlayerName = "Player";
+    public int XP = 0;
+
+    private static string _gameDataFileName = "data.json";
+
+    private static SaveGame _instance;
+    public static SaveGame Instance
+    {
+        get
+        {
+            if (_instance == null)
+                Load();
+            return _instance;
+        }
+
+    }
+
+    public static void Save()
+    {
+        FileManager.Save(_gameDataFileName, _instance);
+    }
+
+    public static void Load()
+    {
+        _instance = FileManager.Load<SaveGame>(_gameDataFileName);
+    }
+
 }
