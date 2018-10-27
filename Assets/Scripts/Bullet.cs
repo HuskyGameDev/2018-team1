d5f49035fe97178109c9afeee723cb37982dad5e
Brandon <bjpaupor@mtu.edu>
2018-10-27 16:01:42 -0400

    altered interpretation of sprite.

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -5,11 +5,18 @@ using UnityEngine;
 public class Bullet : MonoBehaviour {
 
 	public int damage;
-
+    private Rigidbody2D rb;
+    private new Collider2D collider;
+    private new Transform transform;
+    void Start() {
+        transform = GetComponent<Transform>();
+        rb = GetComponent<Rigidbody2D>();
+        collider = GetComponent<Collider2D>();
+    }
 	void OnTriggerEnter2D(Collider2D other) {
         if (other.gameObject.CompareTag("Player")) {
              PlayerInfo player = other.GetComponent<PlayerInfo>();
-			       player.ReduceHealth(damage);
+			 player.ReduceHealth(damage);
              print("hit");
              GameObject.Destroy(this);
 		    }
