873f6c8e46a350e1e23d39f7409179a4f0bd95e7
Crystal Fletcher <sporeluc@gmail.com>
2018-11-02 17:15:23 -0400

    Shaded Toggle Added, Menu & Option Script Created
    Mostly final layout setup (except for volume), and scripts were created with some of the implementation available

---
+++ Assets/Scripts/optionsMenu.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+public class optionsMenu : MonoBehaviour {
+
+	public AudioMixer audioMixer;
+
+	public Dropdown resolutionDropdown;
+
+	Resolution[] resolutions;
+
+	public GameObject backTarget;
+
+	// Use this for initialization
+	void Start () {
+		resolutions = Screen.resolutions;
+		resolutionDropdown.ClearOptions();
+		List<string> options = new List<string>();
+		for ( int i = 0; i < resolutions.Length; i++ )
+		{
+			string option = resolutions[i].width + " x " + resolutions[i].height;
+			options.Add(option);
+		}
+
+		resolutionDropdown.AddOptions(options);
+	}
+
+	public void Back()
+	{
+		this.gameObject.SetActive(false);
+		backTarget.SetActive(true);
+	}
+}
