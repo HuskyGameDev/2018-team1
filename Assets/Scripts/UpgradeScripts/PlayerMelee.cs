6cceceedba8adc7f9f59ecacf12a89ac825570aa
Brandon Paupore <BPaupore>
2019-03-02 15:49:58 -0500

    started new animations for player idle and attacks.

--- Assets/Scripts/UpgradeScripts/PlayerMelee.cs
+++ Assets/Scripts/UpgradeScripts/PlayerMelee.cs
@@ -6,17 +6,21 @@ public class PlayerMelee : MonoBehaviour
 {
     public Collider2D meleeAttack;
     private int attacking;
+    public Animator animator;
     // Update is called once per frame
     void Update()
     {
         if (attacking > 0) {
             attacking--;
-            if (attacking == 0)
+            if (attacking == 0) {
                 meleeAttack.enabled = false;
+                animator.SetBool("Attacking", false);
+            }
         }
         if (Input.GetButtonDown("MeleeAttack")) {
             meleeAttack.enabled = true;
             attacking = 4;
+            animator.SetBool("Attacking", true);
         }
     }
 }
