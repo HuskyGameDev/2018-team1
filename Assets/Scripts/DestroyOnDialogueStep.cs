0a6353257c234e720b8b43bc7dc1186ac24568dc
Brandon Paupore <bjpaupor@mtu.edu>
2019-12-06 19:59:59 -0500

    Made original level give upgrades after reading and to

---
+++ Assets/Scripts/DestroyOnDialogueStep.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOnDialogueStep : MonoBehaviour
+{
+    void Update()
+    {
+        if(Sinput.GetButtonDown("Submit")){
+            Destroy(gameObject);
+        }
+    }
+}
