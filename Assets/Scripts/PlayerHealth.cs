a8791b024ff0d6fa72fadc33b362101c5ace4305
Madeline LeClair <mpleclai@mtu.edu>
2020-02-23 13:42:26 -0500

    fix flatland health bar and modify player prefab to contain health bar instead of text

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -5,12 +5,10 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 public class PlayerHealth : Health
 {
-    //public Text healthText;
     public RectTransform healthBar;
 
     void Update()
     {
-       // healthText.text = "" + string.Format("Health: {0,3}/", GetCurrentHealth()) + maxHealth;
         healthBar.sizeDelta = new Vector2(GetCurrentHealth() * 3, healthBar.sizeDelta.y);
     }
 }
