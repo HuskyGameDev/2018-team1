505df48a08a93833f68478c3bdee1ae2dc1cad97
Devin Farmer <djfarmer@mtu.edu>
2020-03-22 14:38:36 -0400

    Lava rock collision physics and start of W4-1

--- Assets/Scripts/Enemies/LavaRockSpawner.cs
+++ Assets/Scripts/Enemies/LavaRockSpawner.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class LavaRockSpawner : MonoBehaviour
 {
     public GameObject lavaRockPrefab;
-    public float spawnDelay=.3f;
+    public float spawnDelay=.1f;
     public Transform camTransform;
 
     Rigidbody2D rb;
@@ -18,12 +18,18 @@ public class LavaRockSpawner : MonoBehaviour
     }
 
     private void spawnLavaRock(){
-        GameObject lr=Instantiate(lavaRockPrefab) as GameObject;
+        GameObject lr=Instantiate(lavaRockPrefab);
         rb=lr.GetComponent<Rigidbody2D>();
         lr.transform.position=new Vector2(Random.Range(camTransform.position.x-20,camTransform.position.x+20),camTransform.position.y+15);
         Vector2 sideForce=new Vector2(Random.Range(-50,50),0);
         rb.AddForce(sideForce,ForceMode2D.Impulse);
         rb.AddTorque(Random.Range(-20,20),ForceMode2D.Impulse);
+
+        // GameObject child = new GameObject();
+		// child.transform.SetParent(lr.transform);
+        // CircleCollider2D childHitBox = child.AddComponent<CircleCollider2D>();
+		// childHitBox.isTrigger = true;
+		// childHitBox.enabled = false;
     }
 
     IEnumerator spawnRoutine(){
