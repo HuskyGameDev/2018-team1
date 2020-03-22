6a4c68126285c004396bc5d5f45b2faa8ac37eff
Crystal Fletcher <sporeluc@gmail.com>
2020-03-22 13:41:22 -0400

    Created Volume Control Currently need to wait for WWise RTPC addition in order to be able to hook in & control volume

---
+++ Assets/Scripts/Menus/ChangeVolume.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeVolume : MonoBehaviour
+{
+
+	public Slider thisSlider
+	public float masterVolume;
+	public float sfxVolume;
+	public float musicVolume;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		
+	}
+
+	public void SetSpecificVolume(string whatValue)
+	{
+		float sliderValue = thisSlider.value;
+
+		if (whatValue == "Master")
+		{
+			//Debug.Log("changed master volume to :" + thisSlider.value);
+			masterVolume = thisSlider.value;
+			AkSoundEngine.SetRTPCValue("MasterVolume", masterVolume);
+		}
+
+		if (whatValue == "Sounds")
+		{
+			//Debug.Log("changed master volume to :" + thisSlider.value);
+			sfxVolume = thisSlider.value;
+			AkSoundEngine.SetRTPCValue("SFXVolume", sfxVolume);
+		}
+
+		if (whatValue == "Music")
+		{
+			//Debug.Log("changed master volume to :" + thisSlider.value);
+			musicVolume = thisSlider.value;
+			AkSoundEngine.SetRTPCValue("MusicVolume", musicVolume);
+		}
+	}
+}
